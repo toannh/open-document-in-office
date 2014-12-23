f570dd2bb1e63fc815f3404026b8b64471112f7f
toannh <toannh@exoplatform.com>
2014-12-23 16:58:55 +0700

    Update addon

--- webapp/src/main/java/UIOpenDocumentPortlet.java
+++ webapp/src/main/java/UIOpenDocumentPortlet.java
@@ -11,12 +11,6 @@ import java.io.IOException;
 public class UIOpenDocumentPortlet extends GenericPortlet {
 
   @RenderMode(name = "view")
-  public void OpenDocument(RenderRequest request, RenderResponse response) throws IOException, PortletException {
-
-    PortletRequestDispatcher prDispatcher = getPortletContext().getRequestDispatcher("/jsp/hello.jsp");
-
-    prDispatcher.include(request, response);
-
-   }
+  public void OpenDocument(RenderRequest request, RenderResponse response) throws IOException, PortletException {}
 
 }
